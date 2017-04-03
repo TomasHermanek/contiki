d3d5db27f5e888933cf465d10e4dbbb49926e1af
tomhermanek@gmail.com <tomhermanek@gmail.com>
2017-04-03 13:54:41 +0200

    Added functionality for computing target technology

--- apps/heterogeneous-desider/heterogeneous-desider.c
+++ apps/heterogeneous-desider/heterogeneous-desider.c
@@ -28,7 +28,7 @@ LIST(metrics_list);
 MEMB(metrics_memb, struct metrics_struct, MAX_TECHNOLOGIES);
 
 /**
- *
+ * Function allows to add technology into technology table
  * @param type
  */
 tech_struct *add_technology(int type)  {
@@ -44,6 +44,9 @@ tech_struct *add_technology(int type)  {
     return tech;
 }
 
+/**
+ * Function allows to add metrics into Metrics table, which is linked to technology
+ */
 void add_metrics(struct tech_struct *technology, int energy, int bandwidth, int etx) {
     struct metrics_struct *metrics = NULL;
     metrics = memb_alloc(&metrics_memb);
@@ -60,7 +63,7 @@ void add_metrics(struct tech_struct *technology, int energy, int bandwidth, int 
 }
 
 /**
- *
+ * Allows to print Technology Table
  */
 void print_tech_table() {
     struct tech_struct *s;
@@ -72,6 +75,9 @@ void print_tech_table() {
     }
 }
 
+/**
+ * Allows to print Metrics Table
+ */
 void print_metrics_table() {
     struct metrics_struct *s;
 
@@ -80,12 +86,71 @@ void print_metrics_table() {
     }
 }
 
-int select_technology(const void *data) {
+/**
+ * ToDo this section must implement keys extraction from Ondrej part
+ * @param data
+ * @param en
+ * @param bw
+ * @param etx
+ */
+void fill_keys(const void *data, int *en, int *bw, int *etx) {
     int number = (int) data;
-    printf("%s", number);
+    int val = strtol(data, &data, 10);
 
-    // 1 exctract keys data
-    // calculate m
+    if (val % 2 == 0) {
+        *en = 50;
+        *bw = 1;
+        *etx = 1;
+    }
+    else {
+        *en = 1;
+        *bw = 1;
+        *etx = 1;
+    }
+}
+
+/**
+ * Function that implements calculation of metrics
+ *
+ * @param ms
+ * @param k_en
+ * @param k_bw
+ * @param k_etx
+ * @return
+ */
+int calculate_m(struct metrics_struct *ms, int k_en, int k_bw, int k_etx) {
+    return (ms->energy * k_en) + (ms->bandwidth * k_bw) + (ms->etx * k_etx);
+}
+
+/**
+ * Function used for technology selection
+ *
+ * @param data
+ * @return
+ */
+tech_struct *select_technology(const void *data) {
+    int k_en, k_bw, k_etx, m_res = -1;
+    tech_struct *selected_technology = NULL;
+    struct metrics_struct *s;
+
+    fill_keys(data, &k_en, &k_bw, &k_etx);
+    //printf("Kyes %d %d %d\n", k_en, k_bw, k_etx);
+
+    for(s = list_head(metrics_list); s != NULL; s = list_item_next(s)) {
+        //printf("Metrics tech:  %d %d %d\n", s->energy, s->bandwidth, s->etx);
+        if (m_res == -1) {
+            selected_technology = s->technology;
+            m_res = calculate_m(s, k_en, k_bw, k_etx);
+        }
+        else {
+            int m_actual = calculate_m(s, k_en, k_bw, k_etx);
+            if (m_actual < m_res) {
+                selected_technology = s->technology;
+                m_res = calculate_m(s, k_en, k_bw, k_etx);
+            }
+        }
+    }
+    return selected_technology;
 }
 
 /**
@@ -100,7 +165,16 @@ int heterogenous_simple_udp_sendto(struct simple_udp_connection *c,
                                    uint16_t datalen,
                                    const uip_ipaddr_t *to)
 {
-    select_technology(data);
+    // first point check flow table
+    tech_struct *dst_technology;
+    dst_technology = select_technology(data);
+
+    if (dst_technology != NULL) {
+        if (dst_technology->number == RPL_TECHNOLOGY)
+            printf("technology: RPL, %d\n", dst_technology->number);
+        else if (dst_technology->number == WIFI_TECHNOLOGY)
+            printf("technology: WIFI, %d\n", dst_technology->number);
+    }
     // printf("%d\n", sizeof(technology_table) / sizeof(technology_table[0]));
     // check if there is wifi technology
     // 1. exctract metric keys k_en, k_bw, k_etx
@@ -114,8 +188,8 @@ int heterogenous_simple_udp_sendto(struct simple_udp_connection *c,
 void init_module() {
     NETSTACK_MAC.off(1);
 
-    tech_struct *wifi_tech = add_technology(WIFI_TECHNOLOGY);
-    add_metrics(wifi_tech, 40, 1, 1);
+//    tech_struct *wifi_tech = add_technology(WIFI_TECHNOLOGY);
+//    add_metrics(wifi_tech, 40, 1, 1);
     tech_struct *rpl_tech = add_technology(RPL_TECHNOLOGY);
     add_metrics(rpl_tech, 1, 40, 10);
     print_tech_table();
